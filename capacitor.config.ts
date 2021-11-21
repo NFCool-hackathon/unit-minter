069585aafafd313f471ab2466a7e23d2946c1da3
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-21 23:25:16 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nfcool-unit-minter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
