e99c1ca6a52f360cce3ddba625b36a7b396b5c43
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-21 23:51:46 +0100

    chore: infura websockets provider

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,9 @@
+import {infuraKovanProvider} from '../../keys.env';
+
 export const environment = {
   production: false,
   chainId: 42,
+  provider: infuraKovanProvider,
   chainSymbol: 'ETH',
   contractAddress: '0x27a9C18d90EA075bC811D203dF60cc57adA70914'
 };
