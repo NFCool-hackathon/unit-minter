4289ce98269f76de4b2ff13bdc0cd0bd8fd6cf4b
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-21 23:37:49 +0100

    chore: web3 env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  chainId: 42,
+  chainSymbol: 'ETH',
+  contractAddress: '0x27a9C18d90EA075bC811D203dF60cc57adA70914'
 };
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,8 +57,17 @@ import './zone-flags';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
+import * as process from 'process';
 
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
+global.process = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  env: { DEBUG: undefined },
+  version: process.version,
+  nextTick: require('next-tick')
+} as any;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
